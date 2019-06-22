a82d022ef789ce36a8fca300e3d57eadca3ea2b2
Aegis <kacacia24@gmail.com>
2019-06-21 23:35:18 -0400

    Modification de certaine fonction et ajout de l'interaction avec l'utilisateur, la conversion en base 10 fonctionne

--- MemTest.java
+++ MemTest.java
@@ -1,42 +1,86 @@
-
+import java.util.Scanner;
 public class MemTest {
-
-	public static void main(String[] args) {
-		//String  alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		System.out.println(chiffre(42%16));
-		System.out.println(nombre('C'));
-		System.out.println(lire_repr("101010",2));
-		System.out.println(repr_nombre(42,2));
-	}
 	
-	//Prend en entrez un nombre et retourne le charactère associé
+	private static Scanner keyb = new Scanner(System.in);
+	public static void main(String[] args) {
+		int nombre;
+		byte baseNombre;
+		byte baseConversion;
+		boolean verification = false; // Vérifie si la base est bien 2,8,10 ou 16
+       
+        do {
+        	System.out.print("Entrez le nombre a convertir: ");
+        	nombre = keyb.nextInt();
+        	System.out.print("Entrez la base du nombre a convertir: ");
+        	baseNombre = keyb.nextByte();
+        }while(nombre < 0);
+        
+        do {
+	        System.out.print("Entrez la base dans laquelle il doit être convertit: ");
+	        baseConversion = keyb.nextByte();
+	        switch (baseConversion) {
+	        	case 2:
+	        		verification = true;
+	        		break;
+	        	case 8:
+	        		verification = true;
+	        		break;
+	        	case 10:
+	        		verification = true;
+	        		break;
+	        	case 16:
+	        		verification = true;
+	        		break;
+	        	default:
+	        		System.out.println("Base inconnue, veuillez choisir parmi les bases suivantes [2,8,10,16] !");
+	        			
+	        }
+        }while (!verification);
+        
+        if(baseConversion == 10) {
+        	System.out.println(conversionEnBaseDix(nombre, baseNombre));
+        }
+    }
+
+	//Prend en entrer un nombre et retourne le charactère correspondant
 	static char chiffre(int n) {
-		String  alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		return alphaNumeric.charAt(n);
+	        String  alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	        return alphaNumeric.charAt(n);
 	}
-	//Prend en entrez un charactère et retourne le nombre associé
+	//Prend en entrer un charactère et retourne le nombre correspondant
 	static int nombre(char ch) {
-		String  alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		return alphaNumeric.indexOf(ch);
+	        String  alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	        return alphaNumeric.indexOf(ch);
 	}
-	//Prend en entrez une chaine de charactère et sa base est retourne l'entier associé
-	static int lire_repr(String chaine, int base) {
+	//Pour les conversions en base 10
+	/*static int lire_repr(String chaine, int base) {
+	        int entier = 0;
+	        int exp = chaine.length() - 1;
+	        for(int i = 0; i < chaine.length(); i++) {
+	                entier += nombre(chaine.charAt(i)) * Math.pow(base, exp);
+	                exp--;
+	        }
+	        return entier;
+	}*/
+	
+	static int conversionEnBaseDix(int nombre, int base) {
 		int entier = 0;
-		int exp = chaine.length() - 1;
-		for(int i = 0; i < chaine.length(); i++) {
-			entier += nombre(chaine.charAt(i)) * Math.pow(base, exp);
-			exp--;
+		String sx = ""+nombre;  // convertit mon int en String
+		int exp = sx.length() - 1;
+		for(int i = 0; i < sx.length(); i++) {
+            entier += nombre(sx.charAt(i)) * Math.pow(base, exp);
+            exp--;
 		}
 		return entier;
 	}
-
+	//Pour les conversoins en base 2,8,16
 	static StringBuffer repr_nombre(int nombre, int base) {
-		String chaine = "";
-		while(nombre != 0){
-			chaine += chiffre(nombre % base);
-			nombre = nombre / base;
-		}
-		 StringBuffer sb = new StringBuffer(chaine).reverse();
-		return sb;
+	        String chaine = "";
+	        while(nombre != 0){
+	                chaine += chiffre(nombre % base);
+	                nombre = nombre / base;
+	        }
+	         StringBuffer sb = new StringBuffer(chaine).reverse();
+	        return sb;
 	}
 }
